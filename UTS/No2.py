52d5fe1f94879fa9cef8cb112c4b90f152a11591
rafiauliapermana <72639854+rafiauliapermana@users.noreply.github.com>
2022-04-01 10:22:00 +0700

    Create No2.py

---
+++ UTS/No2.py
@@ -0,0 +1,38 @@
+# Nama  : Rafi Aulia Permana
+# NIM   : 20051397071
+# Prodi : D4 Manajemen Informatika (2020A)
+# UTS PRAKTIKUM GRAFIKA KOMPUTER
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import sys
+
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-6, 6, -6, 6, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    glColor3f(1, 1, 1)
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(0.00, 4.00)
+    glVertex2f(4.00, 4.00)
+    glVertex2f(4.00, 0.00)
+    glVertex2f(0.0, 0.00)
+    glVertex2f(0.00, 4.00)
+    glEnd()
+
+    glutSwapBuffers()
+
+
+glutInit(sys.argv)
+glutInitWindowSize(500, 500)
+glutCreateWindow("UTS GRAFIKA KOMPUTER")
+glutDisplayFunc(draw)
+glutMainLoop()
